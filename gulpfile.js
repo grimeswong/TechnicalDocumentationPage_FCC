9573fdb62659c0e97baab7d5f4a135b3826f9965
Shadow Grimes <grimeswongatwork@gmail.com>
2019-02-27 22:38:49 +1000

    define watch html function & add brief descriptions for plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 /*
- * This is an gulf configuration file that setup tasks for automatically run
+ * This is an gulp configuration file that setup tasks for automatically run
  * By convention, a default task(s) should be set with a 'default' keyword
  *
  **/
 
-const { src, dest, watch, parallel, series} = require('gulp');
-const imagemin = require('gulp-imagemin');
-const cleancss = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const sass = require('gulp-sass');
-const browsersync = require('browser-sync').create();
+const { src, dest, watch, parallel, series} = require('gulp');  // plugin for task runner
+const imagemin = require('gulp-imagemin');  // plugin for minify images
+const cleancss = require('gulp-clean-css'); // plugin for minify css
+const autoprefixer = require('gulp-autoprefixer');  // plugin for prefix css
+const sass = require('gulp-sass');  // plugin for convert sass/scss to css
+const browsersync = require('browser-sync').create(); // plugin for live css reload & browser syncing
 
 /*** Source and destination folders ***/
 const srcImg = 'src/img/**/*';  // ** (wildcard) means include all file in current folders and its subfolders
@@ -17,6 +17,8 @@ const destImg = 'public/img';
 const srcCss = 'src/css/**/*.css';
 const srcScss = "src/css/**/*.scss";
 const destCss = 'public/css';
+const srcHtml = 'src/index.html';
+const destHtml = 'public/';
 
 
 function compressimg() {
@@ -50,13 +52,31 @@ function watchsass() {
 }
 
 
+function htmlchange() {
+  return src(srcHtml)
+    .pipe(dest(destHtml))
+    .pipe(browsersync.stream());
+}
+
+/**
+  * This function is for watch the change of html files
+  **/
+function watchhtml() {
+  watch([srcHtml], htmlchange)
+  .on('change', (path, stats) => {
+    console.log(`HTML files in ${path} are copying to public folder ...`);
+    browsersync.reload();
+  });
+}
+
+
 /**
  *  Purpose: This function for reloading the webpage without manualy refresh the browser
  **/
 function livereload() {
   browsersync.init({
     server: {
-      baseDir: "./"
+      baseDir: "./public"
     }
   });
 }
@@ -65,6 +85,7 @@ exports.compressimg = compressimg;  // The name of the tasks runner and export i
 exports.compresscss = compresscss;
 exports.convertsasstocss = convertsasstocss;
 exports.watchsass = watchsass;
+exports.watchhtml = watchhtml;
 exports.livereload = livereload;
 
 /* series() - Combines task functions and/or composed operations into larger operations that will be executed one after another, in sequential order.
@@ -72,5 +93,5 @@ exports.livereload = livereload;
  *
  **/
 
-exports.watch = parallel(watchsass, livereload);
+exports.watch = parallel(watchsass, watchhtml, livereload);
 exports.default = series(watchsass, compressimg);  // Defined a default tasks for executing one after another by using the function "series"

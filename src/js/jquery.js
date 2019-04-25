2a429932cd228fc5f458905f9f54b20c1653f186
Shadow Grimes <grimeswongatwork@gmail.com>
2019-04-26 00:17:14 +1000

    TDP-013: finish off-canvas menu for tablet responsive

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
         $(this).children("i").toggle(".fa-chevron-down");
         $(this).siblings('.nav-sub-links').toggle('fast');
     });
+
+    $('.open-btn-tablet').click(function() {
+        $('#navbar').css({'display':'block', 'left':'0'});
+        $('#main-doc').css('margin-left','15%');
+        $('main').css('background-color','rgba(0,0,0,0.6)')
+    });
+
+    $('.close-btn-tablet').click(function() {
+        $('#navbar').css({'display':'none', 'left':'0'});
+        $('#main-doc').css('margin-left', '0');
+        $('main').css('background-color','rgb(255,255,255)');
+    });
+
 });

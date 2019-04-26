72f1d499a99dce26fb6a73200c1dd98d8f1717b3
Shadow Grimes <grimeswongatwork@gmail.com>
2019-04-26 23:53:05 +1000

    TDP-013: refine the opacity of overlay

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -7,13 +7,18 @@ $(document).ready(function(){
     $('.open-btn-tablet').click(function() {
         $('#navbar').css('left','0');
         $('#main-doc').css('margin-left','15%');
-        $('main').css('background-color','rgba(0,0,0,0.6)')
+        $('#main-doc').addClass('overlay');
+        $('pre').addClass('overlay');
+        $('.section-box-container').addClass('overlay');
+
     });
 
     $('.close-btn-tablet').click(function() {
         $('#navbar').css('left','-40%');
         $('#main-doc').css('margin-left', '0');
-        $('main').css('background-color','rgb(255,255,255)');
+        $('#main-doc').removeClass('overlay');
+        $('pre').removeClass('overlay');
+        $('.section-box-container').removeClass('overlay');
     });
 
 });
